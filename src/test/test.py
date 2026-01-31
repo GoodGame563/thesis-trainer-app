743568bece1aca521cac10f5fd0788bc20af02f2
GoodGame563 <124364912+GoodGame563@users.noreply.github.com>
2026-01-31 12:12:40 +0000

    style: auto-format code with black and isort
    Formatted with:
    - Black 24.4.2
    - isort 5.13.2

--- src/test/test.py
+++ src/test/test.py
@@ -1,5 +1,7 @@
-import sys
 import os
-def hello(  name  ):
-    print(  "Hello "  +name)
-    return   name . strip()
+import sys
+
+
+def hello(name):
+    print("Hello " + name)
+    return name.strip()
